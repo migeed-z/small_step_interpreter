ab3069f201aece9a933fddd14b8d36ae91508ab8
zeina migeed <migeed.z@outlook.com>
2016-08-10 13:47:19 -0400

    change tab size in test file

--- tests/test_step.py
+++ tests/test_step.py
@@ -8,22 +8,26 @@ from Eif import Eif
 
 class TestStep(unittest.TestCase):
 
-  def test_num_no_cont(self):
-      num_expr = ast.Expr(value=Num(3))
-      env = Scope([])
-      k = Done()
-      val = step(num_expr, env, k)[0].value
-      self.assertEqual(val.n, 3)
 
-  def test_num_with_cont(self):
-      num_expr = ast.Expr(value=Num(3))
-      env = Scope([])
-      k = Eif(Expr(value=Num(4)), Expr(value=Num(2)), env, Done())
-      val_tuple = step(num_expr, env, k)
-      val = val_tuple[0].value
-      new_cont = val_tuple[2]
-      self.assertEqual(val.n, 4)
-      self.assertEqual(new_cont, Done())
+    def test_num_no_cont(self):
+        num_expr = ast.Expr(value=Num(3))
+        env = Scope([])
+        k = Done()
+        val = step(num_expr, env, k)[0].value
+        self.assertEqual(val.n, 3)
+
+    def test_num_with_cont(self):
+        num_expr = ast.Expr(value=Num(3))
+        env = Scope([])
+        k = Eif(Expr(value=Num(4)), Expr(value=Num(2)), env, Done())
+        val_tuple = step(num_expr, env, k)
+        val = val_tuple[0].value
+        new_cont = val_tuple[2]
+        self.assertEqual(val.n, 4)
+        self.assertEqual(new_cont, Done())
+
+
+
 
 
 if __name__ == '__main__':
