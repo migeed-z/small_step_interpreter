8e21f8fb32db7d49ab6f77d0d62608ada9bd0a4d
zeina migeed <migeed.z@outlook.com>
2016-08-23 15:12:50 -0400

    add missing file

--- visit.py
+++ visit.py
@@ -1,1 +1,21 @@
-__author__ = 'zeinamigeed'
+from Visitor import visitor
+from ast import Add, Sub, Mult, Div
+
+class OpVisitor:
+    @visitor(Add)
+    def visit(self, op):
+        return '+'
+
+    @visitor(Sub)
+    def visit(self, op):
+        return '-'
+
+    @visitor(Div)
+    def visit(self, op):
+        return '/'
+
+
+    @visitor(Mult)
+    def visit(self, op):
+        return '*'
+
