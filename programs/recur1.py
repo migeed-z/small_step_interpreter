873502b7dd53dfec906a580c94b0ef894396f092
zeina migeed <migeed.z@outlook.com>
2016-08-30 14:48:54 -0400

    y combinator

--- programs/recur1.py
+++ programs/recur1.py
@@ -1,2 +1,1 @@
-
-(lambda f: (lambda x: x(x))(lambda y: f(lambda *args: y(y)(*args))))(lambda f: lambda n: 0 if n == 0 else (1 if n == 1 else f(n-1) + f(n-2)))(6)
+(lambda f: (lambda x: x(x))(lambda y: f(lambda args: y(y)(args))))(lambda f: lambda n: 0 if n == 0 else (1 if n == 1 else f(n-1) + f(n-2)))(2)
